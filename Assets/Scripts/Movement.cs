c49874972be41abea714f2d8f5e4bec01cfe924c
bahram salamat ravandi <bahramsalamat@gmail.com>
2023-02-09 04:13:15 +0100

    release

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float speed = 8f;
-    public float speedMultiplier = 1f;
+    public float speedMultiplier = 1.5f;
     public Vector2 initialDirection;
     public LayerMask obstacleLayer;
 
